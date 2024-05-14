2d65ea71ee2db386001ab29da8f327a0a18f237e
fernandohiroshi <fernandohiroshidev@gmail.com>
2024-05-14 01:30:37 -0300

    nav and readme configs

---
+++ README.md
@@ -0,0 +1,31 @@
+# My Books App
+
+# Installation
+
+After cloning the repository or downloading the ZIP file, run one of the following commands to install the dependencies:
+
+npm install
+
+yarn
+
+pnpm install
+
+## Running the Project
+
+To start the project in development mode, use one of the following commands:
+
+yarn dev
+
+npm run dev
+
+pnpm dev
+
+bun dev
+
+![Preview](./public/preview.png)
+
+These codes create a webpage displaying a list of web development and programming books. They use React with Framer Motion for animations. The BookCard component shows each book with an image, title, description, and a link to get the book. Book data is stored in a file called card-datas.js. The BooksList component organizes the book list on the page using styles for layout. Overall, the codes offer a modular, reusable approach with smooth animations for a better user experience.
+
+[Visit the Live Site](https://hiroshibooks.vercel.app/)
+
+[![](https://skillicons.dev/icons?i=nextjs,tailwind)](https://skillicons.dev)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 import { Variants } from "@/lib/utils";
 import BooksList from "@/components/BooksList";
-import Nav from "@/components/Nav";
 import { ThemeToggle } from "./ThemeToggle";
 
 export default function Home() {
@@ -13,12 +12,20 @@ export default function Home() {
         initial="initial"
         animate="animate"
         variants={Variants}
-        className="top-0 z-50 fixed flex justify-between items-center bg-zinc-100 dark:bg-zinc-950 shadow-lg px-4 lg:px-16 py-4 w-full dark:text-white"
+        className="top-0 z-50 fixed flex justify-between items-center bg-zinc-200/50 dark:bg-zinc-900/60 shadow-lg px-4 lg:px-16 py-4 w-full dark:text-white"
       >
-        <h1 className="font-semibold text-sm md:text-lg lg:text-xl uppercase tracking-widest">
-          Hiroshi Books
-        </h1>
-        <Nav />
+        <a
+          href="#"
+          className="font-semibold text-sm md:text-lg lg:text-xl uppercase tracking-widest"
+        >
+          My Books
+        </a>
+        <a
+          href="https://fernandohiroshi.vercel.app/"
+          className="dark:bg-black/10 bg-white/40 text-black dark:text-white py-1 px-4 rounded-full shadow-md dark:shadow-white/20 shadow-black/20"
+        >
+          Contact
+        </a>
       </motion.header>
       <BooksList />
       <div className="right-4 bottom-4 fixed">
